b1b913c4905bfb37016b810d3d051b2fbc764fda
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-27 16:45:45 +0300

    Added routes and seed data

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BookingsController < ApplicationController
+    protect_from_forgery with: :null_session
     def index
         bookings = Booking.all
         render json: bookings
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'root#index'
   namespace :api do
     namespace :v1 do
-      resources :developers,
+      resources :developers
       resources :bookings
     end
   end
