52ba519050a285d786bb79e525bda120b8a8ba01
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-27 18:23:17 +0300

    Changed index

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BookingsController < ApplicationController
     protect_from_forgery with: :null_session
     def index
-        bookings = Booking.all
+        bookings = Booking.where(user_id: current_user.id)
         render json: bookings
     end
 end
